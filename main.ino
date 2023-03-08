6955f52c4a668d4aea5d3051540c5c7641f6ceee
Ion Mincu <dburner@users.noreply.github.com>
2023-03-08 21:39:15 +0000

    WIP: add switch interrupt

--- main.ino
+++ main.ino
@@ -1,15 +1,35 @@
 #include <Arduino.h>
+#include <avr/sleep.h>
 #include "pulse.h"
 
+// Define pins
+const int switchPin = 3;
+
+void myFunction() {
+  // Read the value of the switch
+  int switchState = digitalRead(switchPin);
+
+  send_commmand();
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize switch pin
+  pinMode(switchPin, INPUT_PULLUP);
+
+  // Configure interrupt for switch pin
+  attachInterrupt(digitalPinToInterrupt(switchPin), myFunction, CHANGE);
+
+  // Configure sleep mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+  // Initialize serial communication
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Sending command");
-  send_commmand();
+  // Serial.println("Entering sleep from loop command");
+  sleep_bod_disable();
 
-  Serial.println("delay");
-  delay(3000);
+  // send_commmand();
+  // delay(3000);
 }
